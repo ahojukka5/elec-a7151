e2a6a734708826cdd5f500f2c67447f1ad4a0c3e
Jukka Aho <ahojukka5@gmail.com>
2022-09-29 20:37:12 +0300

    Add T1-vector_iterators

--- Module_3/T1-vector_iterators/src/vector_it.cpp
+++ Module_3/T1-vector_iterators/src/vector_it.cpp
@@ -7,14 +7,21 @@
 
 std::vector<int> ReadVector() {
   std::vector<int> v;
-  
+  int value;
+  while (std::cin >> value) v.push_back(value);
   return v;
 }
 
 void PrintSum1(const std::vector<int>& v) {
-  
+  for (int i = 1, N = v.size(); i < N; i++) {
+    std::cout << v[i - 1] + v[i] << " ";
+  }
+  std::cout << std::endl;
 }
 
 void PrintSum2(const std::vector<int>& v) {
-  
+  for (int i = 0, N = v.size() / 2; i < N; i++) {
+    std::cout << v[i] + v[v.size() - 1 - i] << " ";
+  }
+  std::cout << std::endl;
 }
