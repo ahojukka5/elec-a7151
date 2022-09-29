ecd86df3c1196ad9eade2ace907518922110633e
Jukka Aho <ahojukka5@gmail.com>
2022-09-29 17:50:52 +0300

    Add T6-vector

--- Module_2/T6-vector/src/vector.cpp
+++ Module_2/T6-vector/src/vector.cpp
@@ -1,18 +1,23 @@
 #include "vector.hpp"
 
+#include <cmath>
+#include <limits>
 #include <vector>
 
 int GetMin(std::vector<int> v) {
-  
-  return 0; 
+  int val = std::numeric_limits<int>::max();
+  for (const int &a : v) val = std::min(val, a);
+  return val;
 }
 
 int GetMax(std::vector<int> v) {
-  
-  return 0; 
+  int val = std::numeric_limits<int>::min();
+  for (const int &a : v) val = std::max(val, a);
+  return val;
 }
 
 double GetAvg(std::vector<int> v) {
-  
-  return 0.0; 
+  double val = 0.0;
+  for (const int &a : v) val += a;
+  return val / v.size();
 }
