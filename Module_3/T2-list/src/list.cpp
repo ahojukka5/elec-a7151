fcb4edc030b6bcc8949a6a7637b0d083d90d2344
Jukka Aho <ahojukka5@gmail.com>
2022-09-29 20:48:56 +0300

    Add T2-list

--- Module_3/T2-list/src/list.cpp
+++ Module_3/T2-list/src/list.cpp
@@ -1,17 +1,29 @@
 #include "list.hpp"
 
+#include <algorithm>
 #include <iostream>
-
-// Implement the functions here
+#include <vector>
 
 std::istream& GetLines(std::istream& is, std::list<std::string>& list) {
-  
+  list.clear();
+  std::string s;
+  do {
+    is >> s;
+    list.push_back(s);
+  } while (is);
+  return is;
 }
 
 void Print(const std::list<std::string>& list) {
-  
+  for (const auto& item : list) {
+    std::cout << item << std::endl;
+  }
 }
 
 void SortAndUnique(std::list<std::string>& list) {
-  
+  std::vector<std::string> vec(list.begin(), list.end());
+  std::sort(vec.begin(), vec.end());
+  vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+  list.clear();
+  for (const auto& item : vec) list.push_back(item);
 }
