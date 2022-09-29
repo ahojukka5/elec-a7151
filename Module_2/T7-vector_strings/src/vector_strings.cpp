01c45f35f305c4972cc68bc5ed1fd1538b39b3ac
Jukka Aho <ahojukka5@gmail.com>
2022-09-29 17:51:05 +0300

    Add T7-vector_strings

--- Module_2/T7-vector_strings/src/vector_strings.cpp
+++ Module_2/T7-vector_strings/src/vector_strings.cpp
@@ -4,17 +4,36 @@
 #include <vector>
 
 void Adder(std::vector<std::string>& names) {
-  
+  std::string s;
+  std::cin >> s;
+  names.push_back(s);
 }
 
 void Remover(std::vector<std::string>& names) {
-  
+  std::cout << names.back() << std::endl;
+  names.pop_back();
 }
 
 void Printer(std::vector<std::string>& names) {
-  
+  for (const auto& s : names) {
+    std::cout << s << std::endl;
+  }
 }
 
 void CMDReader() {
-  
+  std::vector<std::string> names;
+  while (true) {
+    std::cout << "Commands: ADD, PRINT, REMOVE, QUIT" << std::endl;
+    std::cout << "Enter a command:" << std::endl;
+    std::string cmd;
+    std::cin >> cmd;
+    if (cmd == "ADD")
+      Adder(names);
+    else if (cmd == "REMOVE")
+      Remover(names);
+    else if (cmd == "PRINT")
+      Printer(names);
+    else if (cmd == "QUIT")
+      return;
+  }
 }
