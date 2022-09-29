ad22c948c9f712e3699e68313a98864cc447ca3f
Jukka Aho <ahojukka5@gmail.com>
2022-09-29 17:50:11 +0300

    Add T3-simple_class

--- Module_2/T3-simple_class/src/person.cpp
+++ Module_2/T3-simple_class/src/person.cpp
@@ -1,13 +1,8 @@
 #include "person.hpp"
 
-class Person {
- private:
-  const std::string &m_name;
-  const int m_birth_year;
+Person::Person(const std::string &name, int birth_year)
+    : m_name(name), m_birth_year(birth_year) {}
 
- public:
-  Person(const std::string &name, int birth_year)
-      : m_name(name), m_birth_year(birth_year) {}
-  const std::string &GetName() const { return m_name; }
-  int GetAge(int year) const { return year - m_birth_year; };
-};
+const std::string &Person::GetName() const { return m_name; }
+
+int Person::GetAge(int year) const { return year - m_birth_year; };
--- Module_2/T3-simple_class/src/person.hpp
+++ Module_2/T3-simple_class/src/person.hpp
@@ -4,10 +4,14 @@
 #include <iostream>
 
 class Person {
+ private:
+  const std::string &m_name;
+  const int m_birth_year;
+
  public:
   Person(const std::string &name, int birth_year);
   const std::string &GetName() const;
-  int GetAge() const;
+  int GetAge(int year) const;
 };
 
 #endif
