56ac3f6fe256d8b561cb4e778804c0f4564c6dd6
Jukka Aho <ahojukka5@gmail.com>
2022-10-28 11:41:43 +0300

    Add T2-polynomial

--- Module_5/T2-polynomial/src/poly.cpp
+++ Module_5/T2-polynomial/src/poly.cpp
@@ -3,7 +3,56 @@
 #include <sstream>
 
 int Poly::operator[](int exp) const {
-    auto it = values_.find(exp);
-    return it == values_.end() ? 0 : it->second;
+  auto it = values_.find(exp);
+  return it == values_.end() ? 0 : it->second;
 }
 
+std::ostream &operator<<(std::ostream &os, const Poly &p) {
+  bool first = true;
+  for (auto item = p.rbegin(); item != p.rend(); item++) {
+    auto [exp, mul] = *item;
+    if (mul > 0 && !first) os << "+";
+    os << mul << "x" << exp;
+    first = false;
+  }
+  return os;
+}
+
+std::istream &operator>>(std::istream &is, Poly &p) {
+  std::string polystring;
+  is >> polystring;
+  std::stringstream polystream(polystring);
+  int mul, exp;
+  char x;
+  p -= p;
+  while (polystream >> mul >> x >> exp) p[exp] = mul;
+  return is;
+}
+
+Poly operator-(const Poly &p) {
+  Poly c;
+  for (auto [exp, mul] : p) c[exp] = -mul;
+  return c;
+}
+
+Poly &Poly::operator+=(const Poly &b) {
+  for (auto [exp, mul] : b) values_[exp] += mul;
+  return *this;
+}
+
+Poly operator+(const Poly &a, const Poly &b) {
+  Poly c = a;
+  c += b;
+  return c;
+}
+
+Poly &Poly::operator-=(const Poly &b) {
+  for (auto [exp, mul] : b) values_[exp] -= mul;
+  return *this;
+}
+
+Poly operator-(const Poly &a, const Poly &b) {
+  Poly c = a;
+  c -= b;
+  return c;
+}
